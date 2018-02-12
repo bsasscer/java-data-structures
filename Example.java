0b707600ce5406e188148da5182f313f67ff6879
bsasscer <smithbi.maricela@gmail.com>
2018-02-12 12:53:40 -0800

    add comparable interface

--- Example.java
+++ Example.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 import java.util.Date;
 
 import com.teamtreehouse.Treet;
@@ -12,10 +14,21 @@ public class Example {
             "@treehouse course about data structures.",
             new Date(1421849732000L)
         );
+        Treet secondTreet = new Treet(
+            "journeyman",
+            "#treet yoself!",
+            new Date(1421878767000L)
+        );
         System.out.printf("This is a new Treet: %s %n", treet);
         System.out.println("The words are:");
         for (String word : treet.getWords()) {
             System.out.println(word);
+        }
+        Treet[] treets = {secondTreet, treet};
+        Arrays.sort(treets);
+        for (Treet exampleTreet : treets) {
+            System.out.println(exampleTreet);
+
         }
     }
 }
