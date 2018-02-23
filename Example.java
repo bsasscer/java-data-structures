89d3ca7113d6ad60365c1487765461ad1919e56b
bsasscer <smithbi.maricela@gmail.com>
2018-02-23 11:07:39 -0800

    incorporate HashMap interface

--- Example.java
+++ Example.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -21,5 +25,29 @@ public class Example {
         }
         System.out.printf("Hash tags: %s %n", allHashTags);
         System.out.printf("Mentions: %s %n", allMentions);
+
+        Map<String, Integer> hashTagCounts = new HashMap<String, Integer>();
+        for (Treet treet : treets) {
+            for (String hashTag : treet.getHashTags()) {
+                Integer count = hashTagCounts.get(hashTag);
+                if (count == null) {
+                    count = 0;
+                }
+                count++;
+                hashTagCounts.put(hashTag, count);
+            }
+        }
+        System.out.printf("Hash tag counts: %s %n", hashTagCounts);
+
+        Map<String, List<Treet>> treetsByAuthor = new HashMap<String, List<Treet>>();
+        for (Treet treet : treets) {
+            List<Treet> authoredTreets = treetsByAuthor.get(treet.getAuthor());
+            if (authoredTreets == null) {
+                authoredTreets = new ArrayList<Treet>();
+                treetsByAuthor.put(treet.getAuthor(), authoredTreets);
+            }
+            authoredTreets.add(treet);
+        }
+        System.out.printf("Treets by author: %s %n ", treetsByAuthor);
     }
 }
